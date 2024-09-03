521970b56733b5cf263c442f1da4e0af8449d7ab
websiva <siva12022001@gmail.com>
2024-09-03 22:53:36 +0530

    changed layout appearence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'Reydon';
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    // Subscribe to router events and scroll to top on NavigationEnd
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.viewportScroller.scrollToPosition([0, 0]); // Scroll to the top of the page
+      }
+    });
+  }
 }
--- src/app/proximity-google-map/proximity-google-map.component.ts
+++ src/app/proximity-google-map/proximity-google-map.component.ts
@@ -9,13 +9,11 @@ import { Proximity } from '../models/layout-proximity';
 export class ProximityGoogleMapComponent implements OnInit {
 
   @Input() mainLocation: { Latitude: number, Longitude: number } | null = null;
-  @Input() proximityLocations: Proximity[] = [];
 
   center = { lat: 12.9716, lng: 77.5946 }; // Default center location
-  zoom = 11;
+  zoom = 15;
 
   private map!: google.maps.Map;
-  private popup!: Popup;
 
   projectLocation = { lat: 12.9716, lng: 77.5946 };
   proximities: { lat: number, lng: number, name: string, distance: string, duration: string, category: string }[] = [];
@@ -23,6 +21,12 @@ export class ProximityGoogleMapComponent implements OnInit {
   ngOnInit(): void {
     this.initMap();
   }
+
+  mapOptions: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    disableDefaultUI: true,
+    zoomControl: true,
+  };
 
   /** Initializes the map and the custom popup. */
   private initMap(): void {
@@ -43,70 +47,5 @@ export class ProximityGoogleMapComponent implements OnInit {
       map: this.map,
       title: 'Project Location'
     });
-
-    this.popup = new Popup(
-      new google.maps.LatLng(this.projectLocation),
-      document.getElementById('content') as HTMLElement
-    );
-    this.popup.setMap(this.map);
-  }
-}
-
-/**
- * A customized popup on the map.
- */
-class Popup extends google.maps.OverlayView {
-  private position: google.maps.LatLng;
-  private containerDiv: HTMLDivElement;
-
-  constructor(position: google.maps.LatLng, content: HTMLElement) {
-    super();
-    this.position = position;
-
-    content.classList.add('popup-bubble');
-
-    // This zero-height div is positioned at the bottom of the bubble.
-    const bubbleAnchor = document.createElement('div');
-    bubbleAnchor.classList.add('popup-bubble-anchor');
-    bubbleAnchor.appendChild(content);
-
-    // This zero-height div is positioned at the bottom of the tip.
-    this.containerDiv = document.createElement('div');
-    this.containerDiv.classList.add('popup-container');
-    this.containerDiv.appendChild(bubbleAnchor);
-
-    // Optionally stop clicks, etc., from bubbling up to the map.
-    Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
-  }
-
-  /** Called when the popup is added to the map. */
-  override onAdd(): void {
-    this.getPanes()!.floatPane.appendChild(this.containerDiv);
-  }
-
-  /** Called when the popup is removed from the map. */
-  override onRemove(): void {
-    if (this.containerDiv.parentElement) {
-      this.containerDiv.parentElement.removeChild(this.containerDiv);
-    }
-  }
-
-  /** Called each frame when the popup needs to draw itself. */
-  override draw(): void {
-    const divPosition = this.getProjection()?.fromLatLngToDivPixel(this.position);
-
-    // Hide the popup when it is far out of view.
-    const display = divPosition && Math.abs(divPosition.x) < 4000 && Math.abs(divPosition.y) < 4000
-      ? 'block'
-      : 'none';
-
-    if (display === 'block') {
-      this.containerDiv.style.left = `${divPosition!.x}px`;
-      this.containerDiv.style.top = `${divPosition!.y}px`;
-    }
-
-    if (this.containerDiv.style.display !== display) {
-      this.containerDiv.style.display = display;
-    }
   }
 }
