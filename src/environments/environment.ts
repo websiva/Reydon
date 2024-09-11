a96acf9ac3458e04c75a7cb232a7f21e199daa9e
websiva <siva12022001@gmail.com>
2024-09-11 15:19:33 +0530

    proximity icon updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    jsonUrl: 'assets/data/layoutProjects2.json'
+    jsonUrl: 'assets/data/layoutProjects3.json'
     //jsonUrl: 'assets/data/LayoutInputFormat.json' // Local JSON file for development
   };
