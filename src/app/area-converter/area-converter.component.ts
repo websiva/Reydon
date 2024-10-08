02b250f6fa1a4b7ff869d2c5da8b1efc96d158fa
websiva <siva12022001@gmail.com>
2024-10-08 16:28:06 +0530

    area converter logic changed

--- src/app/area-converter/area-converter.component.ts
+++ src/app/area-converter/area-converter.component.ts
@@ -9,14 +9,14 @@ export class AreaConverterComponent {
   unit: string[] = ['Acre', 'Cent', 'Sqft', 'Ground', 'Sq. Mtr', 'Sq. Yrd', 'Hectare'];
 
   units: number[][] = [
-    [1, 100, 43560, 18.15, 4046.85, 4840, 0.40469],  // Acre
-    [0.01, 1, 435.6, 0.1815, 40.46, 48.4, 0.00404],  // Cent
-    [0.00002, 0.002, 1, 0.0004, 0.09, 0.1, 0.000009],  // Sqft
-    [0.05, 5.51, 2400.35, 1, 223, 266.71, 0.02],  // Ground
-    [0.0002, 0.02, 10.76, 0.004, 1, 1.2, 0.0001],  // Sq. Mtr
-    [0.0002, 0.02, 9, 0.003, 0.8, 1, 0.00008],  // Sq. Yrd
-    [2.47, 247.13, 107639, 44.84, 10000, 11959.89, 1]  // Hectare
-  ];
+    [1, 100, 43560, 18.15, 4046.86, 4840, 0.40468],  // Acre
+    [0.01, 1, 435.6, 0.1815, 40.4686, 48.4, 0.004047],  // Cent
+    [0.00002296, 0.002296, 1, 0.0004167, 0.092903, 0.11112, 0.000009291],  // Sqft
+    [0.0551, 5.51, 2400, 1, 222.967, 266.67, 0.022296],  // Ground
+    [0.00024711, 0.02472, 10.76392, 0.004485, 1, 1.196, 0.0001],  // Sq. Mtr
+    [0.00020661, 0.020662, 9, 0.00375, 0.8361, 1, 0.00008361],  // Sq. Yrd
+    [2.4711, 247.105, 107639, 44.8496, 10000, 11959.9005, 1]  // Hectare
+  ];
 
   selectedFromUnit: string = 'Acre'; // Default from unit
   selectedToUnit: string = 'Acre'; // Default to unit
@@ -36,10 +36,16 @@ export class AreaConverterComponent {
 
       // Use the correct indexing for conversion
       const conversionFactor = this.units[fromIndex][toIndex]; // Conversion factor based on selected units
-      this.convertedValue = (this.inputValue * conversionFactor).toFixed(4); // Perform conversion
+       let result = this.inputValue * conversionFactor; // Perform conversion
+       this.convertedValue = this.formatNumber(result);
+
     } catch (error) {
       console.error(error);
       this.convertedValue = 'Invalid units'; // Error handling
     }
+  }
+
+  formatNumber(value:number):string{
+    return parseFloat(value.toFixed(8)).toString();
   }
 }
