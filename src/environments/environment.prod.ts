7bd3e154aaa521a7eeaf14d7e2e462d54d525166
websiva <siva12022001@gmail.com>
2025-02-18 20:51:40 +0530

    source file updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     //jsonUrl: 'https://gist.githubusercontent.com/websiva/0af1545b9c067ab87477357083e9d505/raw/ReydonData.json' // Gist JSON file for production
-    jsonUrl:'https://gist.githubusercontent.com/websiva/3fcdf4d831b09ebadea9c0c502ff30e5/raw/36f100d90049e851727ac73392a0cc366b8766cc/Reydon.json'
+    //jsonUrl:'https://gist.githubusercontent.com/websiva/3fcdf4d831b09ebadea9c0c502ff30e5/raw/36f100d90049e851727ac73392a0cc366b8766cc/Reydon.json'
+    jsonUrl: 'assets/ReydonData.json'
   };
